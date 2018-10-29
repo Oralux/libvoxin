1ef39109e135df639c5317fc4698eb10f020faf0
Gilles Casse <gcasse@oralux.org>
2018-10-29 23:50:02 +0100

    update run.sh

--- src/say/run.sh
+++ src/say/run.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-#cat ../../doc/pg135.txt | head -n 1000 | tail -n $((1000-620)) > /tmp/test.txt
-#./say -f /tmp/test.txt  -w /tmp/test.raw
-./say -w /tmp/test.raw
-sox -r 11025 -e signed -b 16 -c 1 /tmp/test.raw /tmp/test.wav
+unset PLAY
+which aplay && PLAY=aplay
+which paplay && PLAY=paplay
+
+[ -z "$PLAY" ] && echo "Install aplay (alsa-utils) or paplay (pulseaudio-utils)" && exit 1
+
+./say | $PLAY
+
