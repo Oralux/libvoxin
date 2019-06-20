bb73cf4cf9b1cd4ff6d7f5f521c7682a239e8580
Gilles Casse <gcasse@oralux.org>
2019-06-20 23:38:02 +0200

    new defines

--- src/api/voxin.h
+++ src/api/voxin.h
@@ -11,9 +11,13 @@
 typedef enum {voxFemale, voxMale} voxGender;
 typedef enum {voxAdult, voxChild, voxSenior} voxAge;
 
+#define VOX_ECI_VOICES 22
+#define VOX_RESERVED_VOICES 30
+#define VOX_MAX_NB_OF_LANGUAGES (VOX_ECI_VOICES + VOX_RESERVED_VOICES)
 #define VOX_STR_MAX 128
+
 typedef struct {
-  uint32_t id; // voice identifier
+  uint32_t id; // voice identifier, e.g.: 0x2d0002
   char name[VOX_STR_MAX]; // optional: 'Yelda',...
   char lang[VOX_STR_MAX]; // ietf sub tag, iso639-1, 2 letters code: 'en', 'tr',...
   char variant[VOX_STR_MAX]; // ietf sub tag, optional: 'scotland', 'CA',...
@@ -28,7 +32,14 @@ typedef struct {
   char quality[VOX_STR_MAX]; // optional, e.g. "embedded-compact"
 } vox_t;
 
-// return 0 if ok
+// voxGetVoices returns the list of available languages.
+// This functions depreciates eciGetAvailableLanguages.
+// eciGetAvailableLanguages behaves identically if the installed
+// languages come from ECI.
+// Otherwise, eciGetAvailableLanguages returns an unexpected language
+// identifier.
+//
+// Return 0 if ok
 int voxGetVoices(vox_t *list, unsigned int *nbVoices);
 
 // convert the vox_t data to a string in the buffer supplied by the caller (up to len, 0 terminator included)
