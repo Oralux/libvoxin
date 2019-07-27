3eab2b3d9b9fd6262a0d4ec9dd11c2ce232def7a
Gilles Casse <gcasse@oralux.org>
2019-07-27 12:07:09 +0200

    (Cont.) version 1.4.3

--- src/api/voxin.h
+++ src/api/voxin.h
@@ -6,7 +6,7 @@
 
 #define LIBVOXIN_VERSION_MAJOR 1
 #define LIBVOXIN_VERSION_MINOR 4
-#define LIBVOXIN_VERSION_PATCH 2
+#define LIBVOXIN_VERSION_PATCH 3
 
 typedef enum {voxFemale, voxMale} voxGender;
 typedef enum {voxAdult, voxChild, voxSenior} voxAge;
