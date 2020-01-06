2ad36d8f7c07ea80663c9b3524de443abdf76758
Gilles Casse <gcasse@oralux.org>
2020-01-06 10:33:32 +0100

    version 1.4.6 (libinote 1.0.4)

--- src/api/voxin.h
+++ src/api/voxin.h
@@ -6,7 +6,7 @@
 
 #define LIBVOXIN_VERSION_MAJOR 1
 #define LIBVOXIN_VERSION_MINOR 4
-#define LIBVOXIN_VERSION_PATCH 5
+#define LIBVOXIN_VERSION_PATCH 6
 
 typedef enum {voxFemale, voxMale} voxGender;
 typedef enum {voxAdult, voxChild, voxSenior} voxAge;
